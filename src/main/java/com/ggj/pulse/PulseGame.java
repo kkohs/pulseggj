4755e2899278a302914164454f1dd0019162c54d
BrunoLacis <kristaps.kohs@gmail.com>
2013-01-25 20:22:36 +0200

    lib

---
+++ src/main/java/com/ggj/pulse/PulseGame.java
@@ -0,0 +1,11 @@
+package com.ggj.pulse;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Lacis
+ * Date: 13.25.1
+ * Time: 20:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class PulseGame extends Game {
+}
