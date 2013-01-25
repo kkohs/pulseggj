b823ea239de3c221afdf92460dd54c99bf339763
BrunoLacis <kristaps.kohs@gmail.com>
2013-01-25 20:43:02 +0200

    startup

---
+++ src/main/java/com/ggj/pulse/entities/AbstractEntity.java
@@ -0,0 +1,11 @@
+package com.ggj.pulse.entities;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Lacis
+ * Date: 13.25.1
+ * Time: 20:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class AbstractEntity {
+}
