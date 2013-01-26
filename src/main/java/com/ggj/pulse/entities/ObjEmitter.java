f758c235f40a536b3087a8ae4c3b2778f67a5f84
Swiesc <swiesc@gmail.com>
2013-01-26 13:24:20 +0200

    objEmitters

--- src/main/java/com/ggj/pulse/entities/ObjEmitter.java
+++ src/main/java/com/ggj/pulse/entities/ObjEmitter.java
@@ -11,10 +11,15 @@ import java.security.SecureRandom;
  */
 public class ObjEmitter extends ActionEntity {
     private static SecureRandom random = new SecureRandom();
+    private static float maxSize = 5;
+    private static float minSize = 1;
+    private static float speedModifier = 1000000;
 
     private ApplicationContainer applicationContainer;
     private EntityFactory entityFactory;
     private Vector2 direction;
+    private Vector2 vT;
+    private float maxOffset = 3;
 
     private long nextSpawn = 0;
     private long maxCdTime = 10000;
@@ -23,9 +28,12 @@ public class ObjEmitter extends ActionEntity {
     public void update() {
         long t = applicationContainer.getCurrTime();
         if (t >= nextSpawn) {
-            AbstractEntity entity = entityFactory.createBox(getPos().x, getPos().y, 1, 1, 5);
-            Vector2 vel = new Vector2(this.getDirection());
-            vel.mul(random.nextFloat() * 10000);
+            AbstractEntity entity = entityFactory.createBox(getPos().x, getPos().y,
+                    random.nextFloat() * (maxSize - minSize) + minSize, random.nextFloat() * (maxSize - minSize) + minSize, 5);
+            Vector2 vel = new Vector2(vT);
+            vel.mul((random.nextFloat() * maxOffset) - maxOffset / 2);
+            vel.add(getDirection());
+            vel.mul(random.nextFloat() * speedModifier);
             entity.getBody().applyForceToCenter(vel);
             nextSpawn = t + (long) (random.nextFloat() * (float) maxCdTime);
         }
@@ -52,6 +60,7 @@ public class ObjEmitter extends ActionEntity {
     }
 
     public void setDirection(Vector2 direction) {
+        vT = new Vector2(direction.y, -direction.x);
         this.direction = direction;
     }
 
