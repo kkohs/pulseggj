93a19092d7eed6bd3656b90d29973f31ea9bfb36
Swiesc <swiesc@gmail.com>
2013-01-26 17:17:46 +0200

    balls

--- src/main/java/com/ggj/pulse/entities/CircleEntity.java
+++ src/main/java/com/ggj/pulse/entities/CircleEntity.java
@@ -13,22 +13,22 @@ import com.ggj.pulse.utils.AssetManager;
 public class CircleEntity extends AbstractEntity {
     private static Vector3 coords = new Vector3();
     private static Sprite sprite = new Sprite();
-    private String textName = "textures/New Bitmap Image.bmp";
+    private String textName = "textures/New Bitmap Image (2).png";
     private float w, h;
 
     @Override
     public void render(SpriteBatch batch, Camera camera, AssetManager assetManager, float scaleX, float scaleY) {
-        coords.set(getPos().x - w / 2, getPos().y - h / 2, 0);
+        coords.set(getPos().x - w, getPos().y - h, 0);
         camera.project(coords);
         sprite.setTexture(assetManager.get(textName, Texture.class));
-        sprite.setSize(w * scaleX, h * scaleY);
+        sprite.setSize(w * scaleX * 2, h * scaleY * 2);
 
         sprite.setU(0);
         sprite.setV(0);
         sprite.setU2(1);
         sprite.setV2(1);
 
-        sprite.setOrigin(w * scaleX / 2, h * scaleY / 2);
+        sprite.setOrigin(w * scaleX , h * scaleY );
         sprite.setRotation((float) Math.toDegrees(getBody().getAngle()));
         sprite.setPosition(coords.x, coords.y);
 
